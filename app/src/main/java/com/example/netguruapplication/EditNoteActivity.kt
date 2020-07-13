4523e3a57f17e6c49e7dac62cc7d7f516c573bec
Marek <kapsel521@gmail,com>
2020-07-13 16:38:43 +0200

    editNote del button onClick

--- app/src/main/java/com/example/netguruapplication/EditNoteActivity.kt
+++ app/src/main/java/com/example/netguruapplication/EditNoteActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_add_note.*
 import java.lang.Exception
@@ -19,6 +20,7 @@ class EditNoteActivity : AppCompatActivity() {
     private lateinit var currentLists: Button
     private lateinit var archivedLists: Button
     private lateinit var saveToArchive: Button
+    private lateinit var delBtn: Button
     private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +33,7 @@ class EditNoteActivity : AppCompatActivity() {
         savedListBtn = findViewById(R.id.save_list)
         currentLists = findViewById(R.id.current_lists)
         archivedLists = findViewById(R.id.archived_lists)
+        delBtn = findViewById(R.id.delete_button)
 
         idED.setText(MySharedPreferences(this).getIdText())
         titleED.setText(MySharedPreferences(this).getTitleText())
@@ -44,9 +47,23 @@ class EditNoteActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
         archivedLists.setOnClickListener {
             startActivity(Intent(this, ArchivedShoppingListsActivity::class.java))
             finish()
+        }
+
+        delBtn.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are You sure ?")
+                .setMessage("Do You want to delete shopping list ?")
+                .setPositiveButton("Yes") { _, _ ->
+                    delPosition(realm, idED.text.toString().toInt())
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+                .setNegativeButton("No", null)
+                .show()
         }
 
         realm = Realm.getDefaultInstance()
@@ -69,6 +86,21 @@ class EditNoteActivity : AppCompatActivity() {
             finish()
         }catch (e: Exception){
             Toast.makeText(this,"Error $e", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun delPosition(realm: Realm, id: Int): Boolean {
+        return try {
+            realm.beginTransaction()
+            realm.where(Notes::class.java).equalTo("id", id)
+                .findFirst()?.deleteFromRealm()
+            realm.commitTransaction()
+
+            Toast.makeText(this, "List deleted!: $id", Toast.LENGTH_SHORT).show()
+            true
+        }catch (e:Exception){
+            Toast.makeText(this, "not this time $e", Toast.LENGTH_SHORT).show()
+            false
         }
     }
 
